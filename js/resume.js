e194f73052cbf177926808f5e633daba520083e6
jiangl <less.jiang@emc.com>
2018-07-05 14:45:27 +0800

    separate by language

--- js/resume.js
+++ js/resume.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -26,3 +26,23 @@
   });
 
 })(jQuery); // End of use strict
+
+["video_01", "video_02", "video_03", "video_04"]
+.forEach(function (id) {
+  $("#" + id).bind("play", function () {
+    console.log("begin playing " + id);
+    var cid = "#" + id + "_caption";
+    $(cid).fadeOut('slow', null, function () {
+      $(cid).removeClass("d-block");
+      $(cid).addClass("d-none");
+    });
+  });
+  $("#" + id).bind("ended", function () {
+    console.log("end playing " + id);
+    var cid = "#" + id + "_caption";
+    $(cid).fadeIn('fast', null, function () {
+      $(cid).removeClass("d-none");
+      $(cid).addClass("d-block");
+    });
+  });
+});
