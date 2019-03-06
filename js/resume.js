3a8867a3bda8a92a9bc3be12c27cf7ae0969f4de
jiangl <less.jiang@emc.com>
2019-03-06 08:11:02 +0800

    update css

--- js/resume.js
+++ js/resume.js
@@ -50,3 +50,21 @@
 $('.eaddr').click(function(){
   window.location.href='\u004dailto\u003a'+'less\u002ejiang'+'\u0040'+163+'\u002ecom';
 });
+
+
+$("video").click(function(e){
+
+  // handle click if not Firefox (Firefox supports this feature natively)
+  if (typeof InstallTrigger === 'undefined') {
+
+      // get click position 
+      var clickY = (e.pageY - $(this).offset().top);
+      var height = parseFloat( $(this).height() );
+
+      // avoids interference with controls
+      if (clickY > 0.82*height) return;
+
+      // toggles play / pause
+      this.paused ? this.play() : this.pause();
+  }
+});
